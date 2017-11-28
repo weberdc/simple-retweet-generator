50ddb5fadd8bbdd9515675167aaa53a8d0ab7cd3
Derek Weber <weber.dc@gmail.com>
2017-11-28 22:07:05 +1030

    cleaned up imports

--- src/main/java/org/dcw/twitter/generator/TweetCorpusModel.java
+++ src/main/java/org/dcw/twitter/generator/TweetCorpusModel.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Lists;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
